db932c1a3c1be6e89a35044758b68599b4b7a206
Austin Graham <austingraham731@gmail.com>
2019-01-04 13:39:27 -0600

    view for fetching all provider names

--- hamburger/app/product/views.py
+++ hamburger/app/product/views.py
@@ -5,11 +5,17 @@ from pyramid.httpexceptions import HTTPConflict
 from pyramid.httpexceptions import HTTPForbidden
 from pyramid.httpexceptions import HTTPBadRequest
 
+from zope import component
+
 from hamburger.app import AbstractResourceGetView
 from hamburger.app import AbstractAuthenticatedView
 
+from hamburger.dataserver.dataserver.interfaces import IDataserver
+
 from hamburger.dataserver.product.interfaces import IProduct
 from hamburger.dataserver.product.interfaces import IProductCollection
+
+from hamburger.dataserver.provider.interfaces import IProvider
 
 
 @view_config(context=IProductCollection)
@@ -34,3 +40,16 @@ class ProductPostView(AbstractAuthenticatedView):
         if not self.context.insert(item):
             return HTTPConflict()
         return HTTPOk()
+
+
+@view_config(context=IDataserver,
+             request_method="GET",
+             name="providers",
+             renderer="json")
+class ProvidersGetView(AbstractAuthenticatedView):
+
+    def __call__(self):
+        if self.auth_user is None:
+            return HTTPForbidden()
+        providers = component.getUtilitiesFor(IProvider)
+        return [p[0] for p in providers]
